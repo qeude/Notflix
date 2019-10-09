ba64b330c0eb3bad189e8d8d9a084a4dd64eec00
Quentin Eude <quentineude@gmail.com>
2019-10-09 23:09:34 +0200

    Initial commit

---
+++ NetflixLike/Sources/Services/APIService.swift
@@ -0,0 +1,9 @@
+//
+//  APIService.swift
+//  NetflixLike
+//
+//  Created by Quentin Eude on 07/10/2019.
+//  Copyright © 2019 Quentin Eude. All rights reserved.
+//
+
+import Foundation
---
+++ NetflixLike/Sources/View Models/MovieListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListViewModel.swift
+//  NetflixLike
+//
+//  Created by Quentin Eude on 08/10/2019.
+//  Copyright © 2019 Quentin Eude. All rights reserved.
+//
+
+import Foundation
